45dc9475a57ee38436ed9d244c8eaebd5a64fe9a
Bargetor <bargetor@gmail.com>
2014-10-19 16:41:13 +0800

    views modify

---
+++ api/src/APICenter.py
@@ -0,0 +1,8 @@
+POST_DATA_NAME = 'HTTP_RAW_POST_DATA'
+
+class APICenter(object):
+    """docstring for APICenter"""
+    def __init__(self, arg):
+        super(APICenter, self).__init__()
+        self.arg = arg
+
--- api/views.py
+++ api/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from api.src import APIResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    signature = request.GET.get('signature')
-    timestamp = request.GET.get('timestamp')
-    nonce = request.GET.get('nonce')
-    echostr = request.GET.get('echostr')
-    if APIResponse.signature('bargetor_chestnut', signature, timestamp, nonce, echostr):
-        return render(request, 'api/index.html', {'signature' : signature})
-    else:
-        return render(request, 'api/index.html', {'signature' : 'Flash'})
+    if request.TYPE is 'GET':
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        echostr = request.GET.get('echostr')
+        if APIResponse.signature('bargetor_chestnut', signature, timestamp, nonce, echostr):
+            return render(request, 'api/index.html', {'signature' : signature})
+        else:
+            return render(request, 'api/index.html', {'signature' : 'False'})
+        if request.TYPE is 'POST':
+            return HttpResponse(request.POST)
