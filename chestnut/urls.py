8b9c1a253bf2e595f13cf871cbbbe35d1fcd9546
Bargetor <bargetor@gmail.com>
2014-10-20 13:44:27 +0800

    修改api url匹配规则

--- chestnut/urls.py
+++ chestnut/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'chestnut.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^api/', include('api.urls', namespace= 'api')),
+    url(r'^api$', include('api.urls', namespace= 'api')),
     url(r'^admin/', include(admin.site.urls)),
 ]
