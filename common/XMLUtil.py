f090a3ad98871706591f530c70558d81d676174d
Bargetor <bargetor@gmail.com>
2014-10-24 00:03:35 +0800

    写xml实现

--- common/XMLUtil.py
+++ common/XMLUtil.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-import xml.etree.cElementTree as ET
+
+import xml.etree.ElementTree as ET
 import sys
 
 def parse_from_str(xml_str):
@@ -33,6 +33,43 @@ def get_children_text_dic(element, charset = 'utf-8'):
         result[key] = value
     return result
 
+def get_new_etree(root_name):
+    if not root_name:
+        return None
+    root = ET.Element(root_name)
+    etree = ET.ElementTree()
+    etree._setroot(root)
+    return etree
+
+def write_cdata_text_child(root, tag_name, text):
+    e = ET.Element(tag_name)
+    cdata = CDATA(text)
+    e.append(cdata)
+    root.append(e)
+    return root
+
+def write_text_child(root, tag_name, text):
+    e = ET.Element(tag_name)
+    e.text = text
+    root.append(e)
+    return root
+
+def CDATA(text=None):
+    element = ET.Element('![CDATA[')
+    element.text = text
+    return element
+
+ET._original_serialize_xml = ET._serialize_xml
+def _serialize_xml(write, elem, encoding, qnames, namespaces):
+    if elem.tag == '![CDATA[':
+        write("<%s%s]]>" % (
+                elem.tag, elem.text))
+        return
+    return ET._original_serialize_xml(
+        write, elem, encoding, qnames, namespaces)
+ET._serialize_xml = ET._serialize['xml'] = _serialize_xml
+
+
 # reload(sys)
 # sys.setdefaultencoding('utf-8')
 
@@ -41,7 +78,6 @@ def get_children_text_dic(element, charset = 'utf-8'):
 #                                              <FromUserName><![CDATA[fromUser]]></FromUserName>
 #                                              <CreateTime>1348831860</CreateTime>
 #                                              <MsgType><![CDATA[image]]></MsgType>
-#                                              <PicUrl><![CDATA[这是一个图片]]></PicUrl>
 #                                              <MediaId><![CDATA[media_id]]></MediaId>
 #                                              <MsgId>1234567890123456</MsgId>
 #                                          </xml>'''.decode('utf-8'))
@@ -50,4 +86,13 @@ def get_children_text_dic(element, charset = 'utf-8'):
 # print dic['PicUrl']
 
 
-
+# etree = get_new_etree('xml')
+# print etree
+# e = ET.Element('a')
+# e.text = 'b'
+# etree.getroot().append(e)
+# write_cdata_text_child(etree.getroot(), 'c', 'e')
+# print ET.tostring(etree.getroot())
+# etree = parse_from_str(ET.tostring(etree.getroot()))
+# dic = get_children_text_dic(etree)
+# print dic
