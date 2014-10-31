765180dadeadd19e04a1ffcd90d717dc3eaa3b99
Bargetor <bargetor@gmail.com>
2014-10-31 22:40:24 +0800

    ...

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,6 @@
 from api.APISettings import CHARSET
 import sys
-import logging
 
 reload(sys)
 sys.setdefaultencoding(CHARSET)
 
-log = logging.getLogger(__name__)
--- chestnut/__init__.py
+++ chestnut/__init__.py
@@ -1,1 +1,6 @@
 from chestnut.src.ChestnutAPI import ChestnutAPIRequest, ChestnutAPIParser
+from api.APISettings import CHARSET
+import sys
+
+reload(sys)
+sys.setdefaultencoding(CHARSET)
