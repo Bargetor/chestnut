8e02e34541c98421ff6e1eddc21b86ab1ee69394
Bargetor <bargetor@gmail.com>
2014-12-19 20:29:26 +0800

    唉。。。事务管理没思路啊

--- bargetor/common/transaction/Transaction.py
+++ bargetor/common/transaction/Transaction.py
@@ -2,21 +2,35 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 
 class Transaction(object):
     """docstring for Transaction"""
+    def __init(self):
+        self.child_transaction = None
 
-    def is_success():
+    def is_success(self):
         pass
 
-    def is_faile():
+    def is_faile(self):
         pass
 
-    def start():
+    def start(self):
         pass
 
-    def has_child_transaction():
-        pass
+    def has_child_transaction(self):
+        return self.child_transaction is not None
 
-    def get_child_transaction():
-        pass
+    def get_child_transaction(self):
+        return self.child_transaction
+
+    def set_child_transaction(self, transaction):
+        if not isinstance(transaction, Transaction) : return False
+        if not self.has_child_transaction():
+            self.child_transaction = transaction
+        else:
+            self.child_transaction.set_child_transaction(transaction)
+
+class TransactionExecutor(object):
+    """docstring for TransactionExecutor"""
+    def __init__(self):
+        super(TransactionExecutor, self).__init__()
 
 
 
