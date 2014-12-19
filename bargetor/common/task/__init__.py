a2f418f3c9a24f31ce41b27b5c08708944bdedae
Bargetor <bargetor@gmail.com>
2014-12-19 21:29:07 +0800

    想了下，还是建立任务机制

---
+++ bargetor/common/task/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 标记任务, 同异步
+def task(methon = None):
+    pass
+
+# 标记执行，序列号
+def todo(index = 0):
+    pass
+# 标记为验证步， 验证不通过返回执行, 是否只验证一次，循环次数
+def todo_check(index = 0, goto_index = 0, is_once = False, check_count = 10):
+    pass
